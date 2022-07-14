cffe1235e0160ea3c98b528cb7ddcf245fa6f5df
Yoonsoo Kim <laptise@live.jp>
2022-07-14 22:01:22 +0900

    회원가입 페이지 추가

--- client/pages/signup.tsx
+++ client/pages/signup.tsx
@@ -1,25 +1,21 @@
 import { Button, Divider, Paper, Stack, TextField, Typography } from "@mui/material";
-import Link from "next/link";
 import { Layout } from "../components/layout";
 import styles from "../styles/Login.module.scss";
 
-const Login = () => {
+const SignUp = () => {
   return (
-    <Layout mainId="layout" pageTitle="로그인">
+    <Layout mainId="layout" pageTitle="회원가입">
       <Paper id={styles.paper}>
         <Typography variant="h4" color={"primary"}>
-          로그인
+          회원가입
         </Typography>
         <TextField id="email" type={"email"} label="이메일 주소" variant="standard" />
         <TextField id="password" type={"password"} label="암호" variant="standard" />
-        <Button variant="contained">로그인</Button>
-        <Divider />
-        <Link passHref={true} href="/signup">
-          <Button variant="contained">회원가입</Button>
-        </Link>
+        <TextField id="passwordConfirm" type={"password"} label="암호확인" variant="standard" />
+        <Button variant="contained">회원가입</Button>
       </Paper>
     </Layout>
   );
 };
 
-export default Login;
+export default SignUp;
