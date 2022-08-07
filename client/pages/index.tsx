10486275ca24a3e174b4f9fc9629e007d888109d
Yoonsoo Kim <laptise@live.jp>
2022-08-07 22:19:04 +0900

    top preset section

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,12 +1,23 @@
-import { Paper } from "@mui/material";
+import { Box, Paper, Typography } from "@mui/material";
 import type { NextPage } from "next";
 import { Layout } from "../components/layout";
 import styles from "../styles/Index.module.scss";
 
+const TopPreset = () => {
+  return (
+    <Box>
+      <Typography variant="h4">Top Preset</Typography>
+    </Box>
+  );
+};
+
 const Home: NextPage = () => {
   return (
     <Layout pageTitle="홈" mainId="22">
-      <Paper id={styles.paper}>Digging Loops</Paper>
+      <Paper id={styles.paper}>
+        Digging Loops
+        <TopPreset />
+      </Paper>
     </Layout>
   );
 };
