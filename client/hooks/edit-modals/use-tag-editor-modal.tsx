08d42b1036fcae1d58f52b16c0d5255aeafe2c66
Yoonsoo Kim <laptise@live.jp>
2022-09-26 02:12:18 +0900

    에디터 모달

--- client/hooks/edit-modals/use-tag-editor-modal.tsx
+++ client/hooks/edit-modals/use-tag-editor-modal.tsx
@@ -1,29 +1,53 @@
+import { Tag } from "@entities";
 import { Autocomplete, Button, Dialog, DialogTitle, Stack, TextField } from "@mui/material";
 import { useSearchTag } from "gqls";
-import { FC, useState } from "react";
+import { FC, useDeferredValue, useEffect, useState, useTransition } from "react";
 
 type UseTagEditModalProps = {
-  afterSubmit: (value: string) => void;
+  afterSubmit: (tags: Tag[]) => void;
 };
 
 const ModalBody: FC<{ open: boolean; close: () => void } & UseTagEditModalProps> = ({ open, afterSubmit, close }) => {
   const [input, setInput] = useState<string>("");
+  const [isPending, startTransition] = useTransition();
   const { data, loading } = useSearchTag({ name: input });
-  const arr = data?.searchTag || [];
+  const [arr, setArr] = useState<Tag[]>([]);
+  const [pickedTags, setPickedTags] = useState<Tag[]>([]);
+  useEffect(() => {
+    if (!loading && data) {
+      setArr(data?.searchTag || []);
+    }
+  }, [data, loading]);
+
   return (
     <Dialog open={open}>
       <DialogTitle style={{ paddingBottom: 0 }}>{"태그선택"}</DialogTitle>
       <Stack sx={{ p: 2 }}>
-        <Autocomplete
-          options={arr.map((x) => ({ label: x.name, id: x.id }))}
-          renderInput={(params) => <TextField onChange={(e) => setInput(e.target.value)} {...params} label="Movie" />}
+        <Autocomplete<Tag, true>
+          options={arr}
+          multiple
+          onChange={(event, newValue) => setPickedTags(newValue)}
+          filterSelectedOptions
+          getOptionLabel={(option) => option.name}
+          isOptionEqualToValue={(option, value) => option.id === value.id}
+          renderInput={(params) => (
+            <TextField
+              onChange={(e) => {
+                startTransition(() => {
+                  setInput(e.target.value);
+                });
+              }}
+              {...params}
+              label="TAG"
+            />
+          )}
         />
         {/* <TextField variant="outlined" onChange={(e) => setInput(e.target.value)} value={input} /> */}
         <Stack direction="row" spacing={2} sx={{ mt: 2 }}>
           <Button variant="contained" onClick={() => close()}>
             취소
           </Button>
-          <Button disabled={!input} variant="contained" onClick={() => afterSubmit(input)}>
+          <Button disabled={pickedTags?.length > 0 === false} variant="contained" onClick={() => afterSubmit(pickedTags)}>
             확인
           </Button>
         </Stack>
